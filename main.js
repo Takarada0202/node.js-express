ada4a06c7fa5ae21e0a0d1fa8271a974b7b101c3
Takarada0202 <joun0925lim@gmail.com>
2022-01-24 14:12:44 +0900

    라우터주소체계변경

--- main.js
+++ main.js
@@ -33,44 +33,17 @@ app.get('/', function(request, response) {
       `<h2>${title}</h2>${description}
       <img src="/img/hello.jpg" style="width: 500px; display:block;">
       `,
-      `<a href="/create">create</a>`
+      `<a href="/topic/create">create</a>`
     ) 
     response.send(html)
   })
 
-app.get('/page/:pageId',function(request, response, next) {
-    var filteredId = path.parse(request.params.pageId).base
-    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
-      if(err){
-        next(err)
-      } else {
-        var title = request.params.pageId
-        var sanitizedTitle = sanitizeHtml(title)
-        var sanitizedDescription = sanitizeHtml(description, {
-          allowedTags:['h1']
-        })
-        var list = template.list(request.list)
-        var html = template.HTML(sanitizedTitle, list,
-          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-          ` <a href="/create">create</a>
-            <a href="/update/${sanitizedTitle}">update</a>
-            <form action="/delete_process" method="post">
-              <input type="hidden" name="id" value="${sanitizedTitle}">
-              <input type="submit" value="delete">
-            </form>`
-        )
-        response.send(html)
-        
-      }
-    })
-  })
-
-app.get('/create', function(request, response){
+app.get('/topic/create', function(request, response){
 
     var title = 'WEB - create'
     var list = template.list(request.list)
     var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
+      <form action="/topic/create_process" method="post">
         <p><input type="text" name="title" placeholder="title"></p>
         <p>
           <textarea name="description" placeholder="description"></textarea>
@@ -82,21 +55,20 @@ app.get('/create', function(request, response){
     `, '')
     response.send(html)
   })
-
-
-app.post('/create_process', function(request, response) {
-      var post = request.body;
-      console.log(post)
-      var title = post.title;
-      var description = post.description;
-      fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      });
-
-})
-
-app.get('/update/:pageId', function(request, response) {
+  app.post('/topic/create_process', function(request, response) {
+        var post = request.body;
+        console.log(post)
+        var title = post.title;
+        var description = post.description;
+        fs.writeFile(`data/${title}`, description, 'utf8', function(err){
+          response.writeHead(302, {Location: `/?id=${title}`});
+          response.end();
+          response.redirect(`/topic/${title}`)
+        });
+  
+  })
+  
+  app.get('/topic/update/:pageId', function(request, response) {
  
     var filteredId = path.parse(request.params.pageId).base
     fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
@@ -104,7 +76,7 @@ app.get('/update/:pageId', function(request, response) {
       var list = template.list(request.list)
       var html = template.HTML(title, list,
         `
-        <form action="/update_process" method="post">
+        <form action="/topic/update_process" method="post">
           <input type="hidden" name="id" value="${title}">
           <p><input type="text" name="title" placeholder="title" value="${title}"></p>
           <p>
@@ -115,27 +87,26 @@ app.get('/update/:pageId', function(request, response) {
           </p>
         </form>
         `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
+        `<a href="/topic/create">create</a> <a href="/topic/update?id=${title}">update</a>`
       )
       response.send(html)
     })
   })
 
 
-app.post('/update_process', function(request, response) {
+app.post('/topic/update_process', function(request, response) {
           var post = request.body
           var id = post.id
           var title = post.title
           var description = post.description
           fs.rename(`data/${id}`, `data/${title}`, function(error){
             fs.writeFile(`data/${title}`, description, 'utf8', function(err){
-              response.redirect(`/?id=${title}`)
+              response.redirect(`/topic/${title}`)
             })
           })
       })
-//})
 
-app.post('/delete_process', function(request, response) {
+app.post('/topic/delete_process', function(request, response) {
   var post = request.body
   var id = post.id
   var filteredId = path.parse(id).base
@@ -157,44 +128,40 @@ app.listen(port, () => {
 })
 
 
- 
-// app.listen(3000, function() {
-//   console.log('Example app listening on port 3000!')
-// })
-
-
-
-/*
-var http = require('http')
-var fs = require('fs')
-var url = require('url')
-var qs = require('querystring')
-var template = require('./lib/template.js')
-var path = require('path')
-var sanitizeHtml = require('sanitize-html')
-
-var app = http.createServer(function(request,response){
-    var _url = request.url
-    var queryData = url.parse(_url, true).query
-    var pathname = url.parse(_url, true).pathname
-    if(pathname === '/'){
-      if(queryData.id === undefined){
+app.get('/topic/:pageId',function(request, response, next) {
+    var filteredId = path.parse(request.params.pageId).base
+    fs.readFile(`data/${filteredId}`, 'utf8', function(err, description){
+      if(err){
+        next(err)
       } else {
+        var title = request.params.pageId
+        var sanitizedTitle = sanitizeHtml(title)
+        var sanitizedDescription = sanitizeHtml(description, {
+          allowedTags:['h1']
+        })
+        var list = template.list(request.list)
+        var html = template.HTML(sanitizedTitle, list,
+          `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
+          ` <a href="/topic/create">create</a>
+            <a href="/topic/update/${sanitizedTitle}">update</a>
+            <form action="/topic/delete_process" method="post">
+              <input type="hidden" name="id" value="${sanitizedTitle}">
+              <input type="submit" value="delete">
+            </form>`
+        )
+        response.send(html)
+        
       }
-    } else if(pathname === '/create'){
-      
-    } else if(pathname === '/create_process'){
-      
-    } else if(pathname === '/update'){
-      
-    } else if(pathname === '/update_process'){
-      
-    } else if(pathname === '/delete_process'){
-     
-    } else {
-      response.writeHead(404)
-      response.end('Not found')
-    }
-})
-app.listen(3000)
-*/
+    })
+  })
+
+
+
+
+
+
+//})
+
+
+
+
